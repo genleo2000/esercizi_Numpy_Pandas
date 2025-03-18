c012eb02bbe11ee176946570170d4bde2372e389
Gennaro Leo <gennaroleo2000@gmail.com>
2025-03-18 17:44:19 +0100

    Assignment 4 - Esercizio 9

--- esercizio8.py
+++ esercizio8.py
@@ -36,3 +36,27 @@ print(oil_series.sort_index()) # Ordina gli indici in ordine crescente
 print(oil_series.sort_index(ascending=False)) # Ordina gli indici in ordine decrescente
 
 oil_series.sort_values(inplace=True) # con inplace settato a True, la funzione modifica la Series originale
+
+# FUNZIONI ARITMETICHE E OPERAZIONI CON LE STRINGHE
+
+# Aumenta i prezzi del 20% e con il fill_value setta 0 su tutti i valori errati presenti nella Series
+oil_series.multiply(1.2, fill_value=0)
+
+age_data = pd.Series(['Adult 25', 'Child 12', 'Adult 64', 'Teen 17', 'Adult 45'])
+
+# Richiamare "str" per utilizzare metodi di manipolazione delle stringhe
+# expand trasforma il risultato in un DataFrame
+split_data = age_data.str.split(' ', expand=True)
+
+print(split_data[0])
+
+age_groups = pd.Series([
+    'Adult', 'Child', 'Adult', 'Teen', 'Adult', 
+    'Child', 'Teen', 'Adult', 'Adult', 'Adult'
+])
+
+# Calculate the proportion of each age group
+age_group_proportions = age_groups.value_counts(normalize=True)
+
+# Output the proportions
+print(age_group_proportions)
