d73ba0ac0825950bb45666105f3309185559e90d
Gennaro Leo <gennaroleo2000@gmail.com>
2025-03-25 12:20:47 +0100

    DataFrames - Esercizio 10

--- esercizio10.py
+++ esercizio10.py
@@ -16,3 +16,10 @@ print(transaction.info())
 print(transaction.describe())
 
 print(transaction.isna().sum())
+
+# Richiamiamo una colonna del DataFrame, la funzione restituisce una Serie
+print(transaction['store_nbr'])
+
+# Restituisce un DataFrame (anche se avessimo indicato una sola colonna, per via delle parentesi quadre)
+# Le colonne saranno indicate nell'ordine in cui appaiono nella loc
+print(transaction.loc[:5, ['store_nbr', 'date']])
